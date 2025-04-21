fe43b320615a83d4e10fa08038b350d64bf1022d
manhtranduc <manhtranduc0202@gmail.com>
2025-04-21 20:50:18 +0700

    share

--- server/src/routes/document.route.ts
+++ server/src/routes/document.route.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { authenticate } from "../middleware/auth";
 import documentController from "../controllers/document/document.controller";
 import { documentValidator } from "../validators/document.validator";
+import { shareValidator } from "../validators/share.validator";
+import { shareController } from "../controllers/share/share.controller";
 
 const router = Router();
 
@@ -18,17 +20,17 @@ router.put(
 router.post("/", authenticate, documentController.create);
 router.delete("/:id", authenticate, documentController.delete);
 
-// router.post(
-//   "/:id/share",
-//   authenticate,
-//   shareValidator.create,
-//   shareController.create
-// );
+router.post(
+  "/:id/share",
+  authenticate,
+  shareValidator.create,
+  shareController.create
+);
 
-// router.delete(
-//   "/:documentId/share/:userId",
-//   authenticate,
-//   shareController.delete
-// );
+router.delete(
+  "/:documentId/share/:userId",
+  authenticate,
+  shareController.delete
+);
 
 export default router;
