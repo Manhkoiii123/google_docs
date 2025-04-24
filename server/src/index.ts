63a982212e7e3a909cfe97bbb3cb0dbb7ca1afaa
manhtranduc <manhtranduc0202@gmail.com>
2025-04-24 21:19:39 +0700

    fix socket

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,7 @@ import db from "./db/models";
 import env from "./config/env.config";
 import router from "./routes";
 import errorHandler from "./middleware/error-handler";
-import http from "http";
-import { Server } from "socket.io";
-import jwt, { VerifyErrors } from "jsonwebtoken";
-import documentService from "./services/document.service";
-import SocketEvent from "./types/enums/socket-events-enum";
+
 dotenv.config();
 
 const app: Express = express();
@@ -22,76 +18,8 @@ app.use(
   })
 );
 app.use(router);
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: "*",
-  },
-});
+
 const port = env.PORT;
 db.sequelize.sync();
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + Typescript server");
-});
 
-app.listen(port, () => {
-  console.log(`server is listening on port : ${port}`);
-});
-io.on("connection", (socket) => {
-  const accessToken = socket.handshake.query.accessToken as string | undefined;
-  const documentId = socket.handshake.query.documentId as string | undefined;
-
-  if (!accessToken || !documentId) return socket.disconnect();
-  else {
-    jwt.verify(
-      accessToken,
-      "access_token",
-      (err: VerifyErrors | null, decoded: unknown) => {
-        const { id, email } = decoded as RequestUser;
-        (socket as any).username = email;
-
-        documentService
-          .findDocumentById(parseInt(documentId), parseInt(id))
-          .then(async (document) => {
-            if (document === null) return socket.disconnect();
-
-            socket.join(documentId);
-
-            io.in(documentId)
-              .fetchSockets()
-              .then((clients) => {
-                io.sockets.in(documentId).emit(
-                  SocketEvent.CURRENT_USERS_UPDATE,
-                  clients.map((client) => (client as any).username)
-                );
-              });
-
-            socket.on(SocketEvent.SEND_CHANGES, (rawDraftContentState) => {
-              socket.broadcast
-                .to(documentId)
-                .emit(SocketEvent.RECEIVE_CHANGES, rawDraftContentState);
-            });
-
-            socket.on("disconnect", async () => {
-              socket.leave(documentId);
-              socket.disconnect();
-              io.in(documentId)
-                .fetchSockets()
-                .then((clients) => {
-                  io.sockets.in(documentId).emit(
-                    SocketEvent.CURRENT_USERS_UPDATE,
-                    clients.map((client) => (client as any).username)
-                  );
-                });
-            });
-          })
-          .catch((error) => {
-            console.log(error);
-            return socket.disconnect();
-          });
-      }
-    );
-  }
-});
 export default app;
